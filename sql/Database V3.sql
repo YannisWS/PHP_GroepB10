c23135cf979710ceaaf3dc1c7e497d1292d18a5f
YannisWS <yannis.wellemans@hotmail.com>
2019-05-09 11:16:18 +0200

    Update Database

--- sql/Database V3.sql
+++ sql/Database V3.sql
@@ -1,14 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: May 09, 2019 at 09:05 AM
--- Server version: 5.7.21
--- PHP Version: 7.2.7
+-- Host: 127.0.0.1
+-- Gegenereerd op: 09 mei 2019 om 11:15
+-- Serverversie: 10.1.26-MariaDB
+-- PHP-versie: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `projectphp`
@@ -17,7 +25,73 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `posts`
+-- Tabelstructuur voor tabel `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `comment_user_id` int(11) NOT NULL,
+  `comment_text` varchar(500) NOT NULL,
+  `comment_post_id` int(11) NOT NULL,
+  `comment_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `comment_user_id`, `comment_text`, `comment_post_id`, `comment_date`) VALUES
+(1, 8, 'Comment by Test', 1, '2019-05-09 00:00:00'),
+(2, 9, 'Comment by Pol', 1, '2019-05-09 00:00:01');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `friendlist`
+--
+
+CREATE TABLE `friendlist` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `friend_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `friendlist`
+--
+
+INSERT INTO `friendlist` (`id`, `user_id`, `friend_id`) VALUES
+(1, 9, 8),
+(2, 8, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `inappropriate`
+--
+
+CREATE TABLE `inappropriate` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `likes`
+--
+
+CREATE TABLE `likes` (
+  `like_id` int(11) NOT NULL,
+  `like_post_id` int(11) NOT NULL,
+  `like_user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `posts`
 --
 
 CREATE TABLE `posts` (
@@ -27,36 +101,163 @@ CREATE TABLE `posts` (
   `description` varchar(500) NOT NULL,
   `date` datetime NOT NULL,
   `filter` varchar(250) DEFAULT NULL,
-  `location` varchar(250) NOT NULL
+  `location` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indexes for dumped tables
+-- Gegevens worden geëxporteerd voor tabel `posts`
+--
+
+INSERT INTO `posts` (`id`, `file_location`, `post_user_id`, `description`, `date`, `filter`, `location`) VALUES
+(1, '1.jpg', 9, 'Post by Pol', '2019-05-08 00:00:00', NULL, 'Belgium'),
+(2, '1.jpg', 8, 'Post by test', '2019-05-08 00:00:00', NULL, 'Belgium');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `firstname` varchar(250) NOT NULL,
+  `lastname` varchar(250) NOT NULL,
+  `avatar` varchar(250) NOT NULL,
+  `bio` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `firstname`, `lastname`, `avatar`, `bio`) VALUES
+(8, 'test', '$2y$14$iGSvNCkDhczscaQNf2cHI./f.idw9C73qIUwXkPYiMlFrlA0RxV8a', 'test', 'test', 'test', 'test'),
+(9, 'pol', '$2y$14$4l2d5ab4ze.1lOYAQaa8herHt2gn93FqEMn6fnlBxkDnIVVxW.5ju', 'Pol', 'De Tremmerie', 'img/profile/1.png', 'Ik ben pol ');
+
+--
+-- Indexen voor geëxporteerde tabellen
 --
 
 --
--- Indexes for table `posts`
+-- Indexen voor tabel `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `comment_user_id` (`comment_user_id`),
+  ADD KEY `comment_post_id` (`comment_post_id`);
+
+--
+-- Indexen voor tabel `friendlist`
+--
+ALTER TABLE `friendlist`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `friend_id` (`friend_id`);
+
+--
+-- Indexen voor tabel `inappropriate`
+--
+ALTER TABLE `inappropriate`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexen voor tabel `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`like_id`),
+  ADD KEY `like_post_id` (`like_post_id`),
+  ADD KEY `like_user_id` (`like_user_id`);
+
+--
+-- Indexen voor tabel `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`),
   ADD KEY `post_user_id` (`post_user_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexen voor tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
 --
 
 --
--- AUTO_INCREMENT for table `posts`
+-- AUTO_INCREMENT voor een tabel `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT voor een tabel `friendlist`
+--
+ALTER TABLE `friendlist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT voor een tabel `inappropriate`
+--
+ALTER TABLE `inappropriate`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT voor een tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
 
 --
--- Constraints for dumped tables
+-- Beperkingen voor tabel `comments`
 --
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`comment_post_id`) REFERENCES `posts` (`id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`comment_user_id`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `posts`
+-- Beperkingen voor tabel `friendlist`
+--
+ALTER TABLE `friendlist`
+  ADD CONSTRAINT `friendlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `friendlist_ibfk_2` FOREIGN KEY (`friend_id`) REFERENCES `users` (`id`);
+
+--
+-- Beperkingen voor tabel `inappropriate`
+--
+ALTER TABLE `inappropriate`
+  ADD CONSTRAINT `inappropriate_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
+  ADD CONSTRAINT `inappropriate_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Beperkingen voor tabel `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`like_post_id`) REFERENCES `posts` (`id`),
+  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`like_user_id`) REFERENCES `users` (`id`);
+
+--
+-- Beperkingen voor tabel `posts`
 --
 ALTER TABLE `posts`
   ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`post_user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
