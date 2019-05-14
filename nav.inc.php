0c5e25731bcca1cb7a4f22c7d8c530551600da58
AmelieGosiau <amelie.gosiau@hotmail.com>
2019-05-14 15:15:36 +0200

    Update nav.inc.php
    Update

--- nav.inc.php
+++ nav.inc.php
@@ -15,10 +15,7 @@
     </a>
 
     <a href="Imgupload.php">Upload</a>
-    <a href="profile.php">Profile <div class="bullet" style="display:
-    <?php if(count(Notification::getUnseen())== 0){
-        
-        echo('none');}?>"></div></a>
+    <a href="profile.php">Profile </a>
     <a href="logout.php">Logout</a>
     <form action="search.php" method="get">
         <input type="text" name="search" id="searchfield" placeholder="Search">
